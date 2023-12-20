f6379c11492dcf58fa9f6649ce88e68dfe51f908
dylanhamada <dylan.hamada@gmail.com>
2023-12-20 10:58:28 -0500

    Conditionally Render Sold Out Icon on Product Page
    - Conditionally render a div reading "Sold Out" if product.quantity is zero

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,9 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @products.each do |product|
+      Rails.logger.debug product.inspect
+    end
   end
 
   def show
