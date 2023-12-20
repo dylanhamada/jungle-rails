4a8524373e1e90f52fe40c44923e8b80246e8def
dylanhamada <dylan.hamada@gmail.com>
2023-12-20 17:17:34 -0500

    Display Product and Category Total on Admin Dashboard
    - Modify show action in dashboard_controller.rb to pass total products and categories
    - Display total products and categories in show.html.erb

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
+    puts @total_categories
   end
 end
