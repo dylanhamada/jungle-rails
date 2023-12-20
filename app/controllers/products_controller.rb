527bddd4639de5c7dba9a886b9a406954108c4bd
dylanhamada <dylan.hamada@gmail.com>
2023-12-20 11:00:01 -0500

    Empty commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,6 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
-    @products.each do |product|
-      Rails.logger.debug product.inspect
-    end
   end
 
   def show
